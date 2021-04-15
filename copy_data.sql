1e8c8aa50800157a7999c7e2b5d0edb5958640d1
zdliu <48446091+zdliu2021@users.noreply.github.com>
2021-04-15 20:13:03 +0800

    Update copy_data.sql

--- copy_data.sql
+++ copy_data.sql
@@ -4,7 +4,7 @@
 
 
 
-### dbo
+-- dbo
 
 \copy "dbo.awbuildversion" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
@@ -12,7 +12,71 @@
 
 \copy "dbo.errorlog" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
+--humanresources
 
+\copy "humanresources.department" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "humanresources.employeedepartmenthistory" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "humanresources.jobcandidate" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "humanresources.shift" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+--person
+
+\copy "person.address" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.addresstype" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.businessentity" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.businessentityaddress" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.businessentitycontact" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.contacttype" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.countryregion" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.emailaddress" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.password" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.person" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.personphone" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.phonenumbertype" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "person.stateprovince" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+--production
+
+\copy "production.billofmaterials" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "production.culture" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "production.document" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "production.illustration" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "production.productcategory" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "production.productdescription" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "production.productdocument" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "production.productinventory" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "production.productmodel" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+
+
+\copy "production.productmodelillustration" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+
+\copy "production.productmodelproductdescriptionculture" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
 \copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
@@ -28,43 +92,14 @@
 
 \copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
+\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
+\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 \copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
-
-\copy "humanresources.employee" from '/home/lzd/employee.csv' with csv header encoding 'utf8';
 
