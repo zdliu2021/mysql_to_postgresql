d56832c78079ada11d26abb27f8e2c1513488f7f
zdliu <48446091+zdliu2021@users.noreply.github.com>
2021-04-19 15:22:19 +0800

    Update practice_postgresql.sql

--- practice_postgresql.sql
+++ practice_postgresql.sql
@@ -1,10 +1,10 @@
 1.在Product表中查出所有产品信息（输出格式：Name, ProductNumber, ListPrice），按产品名升序排列。
 
-select Name,ProductNumber,ListPrice
+select "Name","ProductNumber","ListPrice"
 
 from product
 
-order by product.Name asc;
+order by product."Name" asc;
 
 
 
@@ -13,37 +13,37 @@ order by product.Name asc;
 2.查出每种产品的销售总额和折扣总额。涉及表：product、salesorderdetail，按产品名升序排列。输出格式(Name, LineTotal, discount[OrderQty*UnitPriceDiscount]))。
 
 
-select Name , LineTotal, OrderQty*UnitPriceDiscount as discount
+select "Name" , "LineTotal", "OrderQty" * "UnitPriceDiscount" as discount
 
 from product join salesorderdetail
 
-on product.ProductID=salesorderdetail.ProductID
+on product."ProductID"=salesorderdetail."ProductID"
 
-order by Name asc;
+order by "Name" asc;
 
 
 
 
 3.查出每种产品类型均价等于该类产品最高标价的产品类型号。涉及表：Product。输出格式为(ProductModelID)
 
-select ProductModelID
+select "ProductModelID"
 
 from product
 
-group by product.ProductModelID
+group by product."ProductModelID"
 
-having  MAX(ListPrice)=avg(ListPrice);
+having  MAX("ListPrice")=avg("ListPrice");
 
 
 
 
 4.查出每个销售订单的销售总额。涉及表：SalesOrderDetail。输出格式为(SalesOrderID,total[=sum(LineTotal)])
 
-select salesorderdetail.SalesOrderID,sum(LineTotal) as total
+select salesorderdetail."SalesOrderID",sum("LineTotal") as total
 
 from salesorderdetail
 
-group by SalesOrderID;
+group by "SalesOrderID";
 
 
 
@@ -51,46 +51,46 @@ group by SalesOrderID;
 
 5.查出标价大于$1000的产品，求出这些产品的均价(按ProductModelID分组)。涉及表：Product。输出格式为(ProductModelID,avg_price[avg(ListPrice)])
 
-select ProductModelID,
+select "ProductModelID",
 
-avg(ListPrice)
+avg("ListPrice")
 
 from product
 
-where ListPrice>1000
+where "ListPrice">1000
 
-group by ProductModelID;
+group by "ProductModelID";
 
 
 
 6.查出总销量大于5的产品ID，并按总销量排序。涉及表：salesorderdetail。输出格式为(ProductID,count[=sum(OrderQty])
 
-select ProductID,
+select "ProductID",
 
-sum(OrderQty) as count
+sum("OrderQty") as count
 
 from salesorderdetail
 
-group by ProductID
+group by "ProductID"
 
-having sum(OrderQty)>5
+having sum("OrderQty")>5
 
-order by sum(OrderQty) desc;
+order by sum("OrderQty") desc;
 
 
 7.查出总销售额大于$10000且平均单笔销量小于3的产品。三列：ID、均价、总销售额。涉及表：SalesOrderDetail。输出格式为(ProductID,avg_price,sum_LineTotal)
 
-select ProductID,
+select "ProductID",
 
-avg(UnitPrice) avg_price,sum(LineTotal) as sum_LineTotal
+avg("UnitPrice") avg_price,sum("LineTotal") as sum_LineTotal
 
 from salesorderdetail
 
-group by ProductID
+group by "ProductID"
 
-having avg(OrderQty)<3
+having avg("OrderQty")<3
 
-and sum(LineTotal)>10000;
+and sum("LineTotal")>10000;
 
 
 
@@ -98,37 +98,37 @@ and sum(LineTotal)>10000;
 
 涉及SalesOrderDetail，Product两个表
 
-SELECT p.Name AS ProductName, 
+SELECT p."Name" AS ProductName, 
 
-OrderQty * UnitPrice as NonDiscountSales,
+"OrderQty" * "UnitPrice" as NonDiscountSales,
 
-((OrderQty * UnitPrice) * UnitPriceDiscount) as Discounts
+(("OrderQty" * "UnitPrice") * "UnitPriceDiscount") as Discounts
 
 FROM product AS p 
 
 INNER JOIN salesorderdetail AS sod
 
-ON p.ProductID = sod.ProductID 
+ON p."ProductID" = sod."ProductID"
 
-ORDER BY ProductName DESC;
+ORDER BY "Name" DESC;
 
 
 
 9.查询所有名字以“Chain”开头的ProductID,Name
 
-SELECT ProductID, Name
+SELECT "ProductID", "Name"
 
 FROM product
 
-WHERE Name LIKE 'Chain%'; 
+WHERE "Name" LIKE 'Chain%'; 
 
 
 
 10.从Person表中选出MiddleName中包含E或B字符的the business entity ID number, first name, middle name, 和 last name
 
-SELECT BusinessEntityID, FirstName, MiddleName, LastName
+SELECT "BusinessEntityID", "FirstName", "MiddleName", "LastName"
 
 FROM person
 
-WHERE MiddleName LIKE '%E%' or MiddleName Like '%B%'; 
+WHERE "MiddleName" LIKE '%E%' or "MiddleName" Like '%B%'; 
                                                                    
