8f73fbaf6a716cc800e4e4bb67c636854caad682
zdliu <48446091+zdliu2021@users.noreply.github.com>
2021-05-04 21:31:30 +0800

    Update new.sql

--- new.sql
+++ new.sql
@@ -1,1 +1,30 @@
 
+salesorderdetail:(SalesOrderID,
+                 SalesOrderDetailID,
+                 CarrierTrackingNumber,
+                 OrderQty,
+                 ProductID,
+                 SpecialOfferID,
+                 UnitPrice,
+                 UnitPriceDiscount,
+                 LineTotal,
+                 rowguid,)
+
+salesorderheader:(SalesOrderID,
+                 RevisionNumber,
+                 orderDate,
+                 DueDate,
+                 ShipDate,
+                 Status,
+                 onlineOrderFlag,
+                 SalesOrderNumber,
+                 PurchaseOrderNumber,
+                 AccountNumber,
+                 CustomerID,
+                 SalesPersonID,
+                 )
+select SalesPersonID
+from salesorderheader join salesorderdetail
+where salesorderdetail.SalesOrderID=salesorderheader.SalesOrderID
+group by SalesPersonID
+by 
