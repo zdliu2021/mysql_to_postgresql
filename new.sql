0b819ef4dc1437fca7b7963c6357cc2abdd513de
zdliu <48446091+zdliu2021@users.noreply.github.com>
2021-05-08 22:51:13 +0800

    Update new.sql

--- new.sql
+++ new.sql
@@ -34,11 +34,28 @@ address:(AddressID,
 -- dense_rank() over (partition by city
 -- order by _count desc )as rank
 
-select city,SalesPersonID
+with temp_1 as(select city,SalesPersonID,_count
 from (select SalesPersonID,sum(LineTotal) as _count
 from salesorderheader inner join salesorderdetail
 where salesorderdetail.SalesOrderID=salesorderheader.SalesOrderID
 group by SalesPersonID) Temp join businessentityaddress join address 
 where Temp.SalesPersonID=businessentityaddress.BusinessEntityID 
-and businessentityaddress.AddressID=address.AddressID
+and businessentityaddress.AddressID=address.AddressID)
+
+select city,SalesPersonID
+from temp_1 a
+where (select count(1) from temp_1 b where a.city=b.city and a._count<b._count )<3;
+
+select city,SalesPersonID
+from (select city,SalesPersonID,_count
+from (select SalesPersonID,sum(LineTotal) as _count
+from salesorderheader inner join salesorderdetail
+where salesorderdetail.SalesOrderID=salesorderheader.SalesOrderID
+group by SalesPersonID) Temp join businessentityaddress join address 
+where Temp.SalesPersonID=businessentityaddress.BusinessEntityID 
+and businessentityaddress.AddressID=address.AddressID) temp_1
+where (select count(1) from temp_1 b where a.city=b.city and a._count<b._count )<3;
+
+
+
 
