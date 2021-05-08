6ece2c3e6481a23fbecb1059fb4beef2c2a665ef
zdliu <48446091+zdliu2021@users.noreply.github.com>
2021-05-08 22:55:56 +0800

    Update new.sql

--- new.sql
+++ new.sql
@@ -33,6 +33,8 @@ address:(AddressID,
 3.
 -- dense_rank() over (partition by city
 -- order by _count desc )as rank
+定义每个员工参与的所有订单的总销售额(sum(LineTotal))作为员工的业绩，列出每个城市的业绩排名前三的员工,涉及表(salesorderheader,salesorderdetail,address,businessentutyaddress)。
+                       输出格式为(city,SalesPersonID)
 
 with temp_1 as(select city,SalesPersonID,_count
 from (select SalesPersonID,sum(LineTotal) as _count
@@ -45,17 +47,6 @@ and businessentityaddress.AddressID=address.AddressID)
 select city,SalesPersonID
 from temp_1 a
 where (select count(1) from temp_1 b where a.city=b.city and a._count<b._count )<3;
-
-select city,SalesPersonID
-from (select city,SalesPersonID,_count
-from (select SalesPersonID,sum(LineTotal) as _count
-from salesorderheader inner join salesorderdetail
-where salesorderdetail.SalesOrderID=salesorderheader.SalesOrderID
-group by SalesPersonID) Temp join businessentityaddress join address 
-where Temp.SalesPersonID=businessentityaddress.BusinessEntityID 
-and businessentityaddress.AddressID=address.AddressID) temp_1
-where (select count(1) from temp_1 b where a.city=b.city and a._count<b._count )<3;
-
 
 
 
